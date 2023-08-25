2961cfbd3c474ed0c608377612f7e92fdb6b1dc0
Ariel Ulagnero <ariel@MacBook-Pro-de-Ariel.local>
2023-08-25 18:21:09 -0400

    commit final del cogigo completo

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,175 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       calificacion={peliculas[0].calificacion}
       votos={peliculas[0].votos}
     ></Pelicula>
-    
+
+    <Pelicula
+      enlaceImdb={peliculas[1].enlaceImdb}
+      afiche={peliculas[1].afiche}
+      titulo={peliculas[1].titulo}
+      sinopsis={peliculas[1].sinopsis}
+      calificacion={peliculas[1].calificacion}
+      votos={peliculas[1].votos}
+    ></Pelicula>
+
+    <Pelicula
+      enlaceImdb={peliculas[2].enlaceImdb}
+      afiche={peliculas[2].afiche}
+      titulo={peliculas[2].titulo}
+      sinopsis={peliculas[2].sinopsis}
+      calificacion={peliculas[2].calificacion}
+      votos={peliculas[2].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[3].enlaceImdb}
+      afiche={peliculas[3].afiche}
+      titulo={peliculas[3].titulo}
+      sinopsis={peliculas[3].sinopsis}
+      calificacion={peliculas[3].calificacion}
+      votos={peliculas[3].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[4].enlaceImdb}
+      afiche={peliculas[4].afiche}
+      titulo={peliculas[4].titulo}
+      sinopsis={peliculas[4].sinopsis}
+      calificacion={peliculas[4].calificacion}
+      votos={peliculas[4].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[5].enlaceImdb}
+      afiche={peliculas[5].afiche}
+      titulo={peliculas[5].titulo}
+      sinopsis={peliculas[5].sinopsis}
+      calificacion={peliculas[5].calificacion}
+      votos={peliculas[5].votos}
+    ></Pelicula>
+    <Pelicula
+      enlaceImdb={peliculas[6].enlaceImdb}
+      afiche={peliculas[6].afiche}
+      titulo={peliculas[6].titulo}
+      sinopsis={peliculas[6].sinopsis}
+      calificacion={peliculas[6].calificacion}
+      votos={peliculas[6].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[7].enlaceImdb}
+      afiche={peliculas[7].afiche}
+      titulo={peliculas[7].titulo}
+      sinopsis={peliculas[7].sinopsis}
+      calificacion={peliculas[7].calificacion}
+      votos={peliculas[7].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[8].enlaceImdb}
+      afiche={peliculas[8].afiche}
+      titulo={peliculas[8].titulo}
+      sinopsis={peliculas[8].sinopsis}
+      calificacion={peliculas[8].calificacion}
+      votos={peliculas[8].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[9].enlaceImdb}
+      afiche={peliculas[9].afiche}
+      titulo={peliculas[9].titulo}
+      sinopsis={peliculas[9].sinopsis}
+      calificacion={peliculas[9].calificacion}
+      votos={peliculas[9].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[10].enlaceImdb}
+      afiche={peliculas[10].afiche}
+      titulo={peliculas[10].titulo}
+      sinopsis={peliculas[10].sinopsis}
+      calificacion={peliculas[10].calificacion}
+      votos={peliculas[10].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[11].enlaceImdb}
+      afiche={peliculas[11].afiche}
+      titulo={peliculas[11].titulo}
+      sinopsis={peliculas[11].sinopsis}
+      calificacion={peliculas[11].calificacion}
+      votos={peliculas[11].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[12].enlaceImdb}
+      afiche={peliculas[12].afiche}
+      titulo={peliculas[12].titulo}
+      sinopsis={peliculas[12].sinopsis}
+      calificacion={peliculas[12].calificacion}
+      votos={peliculas[12].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[13].enlaceImdb}
+      afiche={peliculas[13].afiche}
+      titulo={peliculas[13].titulo}
+      sinopsis={peliculas[13].sinopsis}
+      calificacion={peliculas[13].calificacion}
+      votos={peliculas[13].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[14].enlaceImdb}
+      afiche={peliculas[14].afiche}
+      titulo={peliculas[14].titulo}
+      sinopsis={peliculas[14].sinopsis}
+      calificacion={peliculas[14].calificacion}
+      votos={peliculas[14].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[15].enlaceImdb}
+      afiche={peliculas[15].afiche}
+      titulo={peliculas[15].titulo}
+      sinopsis={peliculas[15].sinopsis}
+      calificacion={peliculas[15].calificacion}
+      votos={peliculas[15].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[16].enlaceImdb}
+      afiche={peliculas[16].afiche}
+      titulo={peliculas[16].titulo}
+      sinopsis={peliculas[16].sinopsis}
+      calificacion={peliculas[16].calificacion}
+      votos={peliculas[16].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[17].enlaceImdb}
+      afiche={peliculas[17].afiche}
+      titulo={peliculas[17].titulo}
+      sinopsis={peliculas[17].sinopsis}
+      calificacion={peliculas[17].calificacion}
+      votos={peliculas[17].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[18].enlaceImdb}
+      afiche={peliculas[18].afiche}
+      titulo={peliculas[18].titulo}
+      sinopsis={peliculas[18].sinopsis}
+      calificacion={peliculas[18].calificacion}
+      votos={peliculas[18].votos}
+    ></Pelicula>
+
+<Pelicula
+      enlaceImdb={peliculas[19].enlaceImdb}
+      afiche={peliculas[19].afiche}
+      titulo={peliculas[19].titulo}
+      sinopsis={peliculas[19].sinopsis}
+      calificacion={peliculas[19].calificacion}
+      votos={peliculas[19].votos}
+    ></Pelicula>
   </React.StrictMode>,
 )
