2e76cfcef2230df073ce21840d9bdbf838e74573
Ariel Ulagnero <ariel@MacBook-Pro-de-Ariel.local>
2023-08-24 23:19:26 -0400

    arreglé la foto, pero no funciona flex

--- src/pelicula.jsx
+++ src/pelicula.jsx
@@ -4,20 +4,25 @@ export function Pelicula(){
             <aside>
                 
                 <a href="https://www.imdb.com/title/tt9362722" target="_blank">
-                    <img src="https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_UY209_CR0,0,140,209_AL_.jpg" alt="Spider-Man a Través del Spider-Verso"/>
+                    <img src="./afiches/spiderverse.jpg" alt="Spider-Man a Través del Spider-Verso"/>
                 </a>
 
 
             </aside>
             <section>
-                <div><h1>Spider-Man a través del Spider-verso</h1></div>
                 <div>
-                    <p><span class="bold">Sinopsis: </span>Miles Morales emprende una aventura a través
+                    <a href="https://www.imdb.com/title/tt9362722" target="_blank">
+                    <h1>Spider-Man a través del Spider-verso</h1>
+                    </a>
+                    
+                </div>
+                <div>
+                    <p><span className="bold">Sinopsis: </span>Miles Morales emprende una aventura a través
                 del multiverso con Gwen Stacy y un nuevo equipo de la Spider People que deben enfrentar a un poderoso
                 villano.</p>
                 </div>
                 <div>
-                    <p><span class="bold">Calificación: </span><b>8.9 </b> (Votos: 191.550)</p>
+                    <p><span className="bold">Calificación: </span><b>8.9 </b> (Votos: 191.550)</p>
                     </div>
             </section>
         </section>
